8cc55e0683b22fc4b5e663d1972a10191fd04a62
Arsenij Kudravcev <ars.manag@yandex.ru>
2023-11-01 20:24:48 +0300

    modified:   task-1/bin/surf_dart_courses_template.dart

--- task-1/bin/surf_dart_courses_template.dart
+++ task-1/bin/surf_dart_courses_template.dart
@@ -1,42 +1,33 @@
-class nullBody {
-  final String type;
-  final String brain;
-  final String heart;
-  String? appendix;
-  nullBody({
-    required this.type,
-    required this.brain,
-    required this.heart,
-  });
-
-  @override
-  String toString() {
-    var aboutMe = ''' Type: $type Brain: $brain Heart: $heart ''';
-    return aboutMe;
-  }
-
-  void live() {
-    print('$type is living');
-  }
-}
-
-class water extends nullBody {
-  water({
-    required super.type,
-    required super.brain,
-    required super.heart,
-  });
-
-  void swim() {
-    print('$type is swimming');
-  }
-}
-
 void main() {
-  final human = nullBody(type: "Human", brain: "yes", heart: "yes");
-  print(human);
-  human.live();
-  final fish = water(type: "salmon", brain: "small", heart: "yes");
-  print(fish);
-  fish.swim();
+  bool sleepness = true;
+  String milk = '';
+  String market = '';
+
+  while (sleepness == true) {
+    if (milk == "have") {
+      boilWater();
+      makeCoffe();
+      goToWork();
+    } else
+      goToMarket();
+
+    if (market == 'close') {
+      cry();
+      goToWork();
+    } else {
+      buyMilk();
+      goHome();
+      boilWater();
+      makeCoffe();
+      goToWork();
+    }
+  }
 }
+
+boilWater() {}
+makeCoffe() {}
+goToWork() {}
+goToMarket() {}
+cry() {}
+buyMilk() {}
+goHome() {}
