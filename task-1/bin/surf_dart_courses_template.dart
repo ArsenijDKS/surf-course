a80a15ce6d042cd088d6f438a52e239f1be17177
Arsenij Kudravcev <ars.manag@yandex.ru>
2023-11-27 20:30:32 +0300

    modified:   task-1/bin/surf_dart_courses_template.dart 	modified:   task-1/lib/surf_dart_courses_template.dart

--- task-1/bin/surf_dart_courses_template.dart
+++ task-1/bin/surf_dart_courses_template.dart
@@ -1,43 +1,159 @@
-import 'package:surf_dart_courses_template/surf_dart_courses_template.dart'
-    as surf_dart_courses_template;
+import 'package:surf_dart_courses_template/surf_dart_courses_template.dart';
 
-void main(List<String> arguments) async {
-  final articles = '''
-1,хлеб,Бородинский,500,5
-2,хлеб,Белый,200,15
-3,молоко,Полосатый кот,50,53
-4,молоко,Коровка,50,53
-5,вода,Апельсин,25,100
-6,вода,Бородинский,500,5
-''';
+void main(List<String> arguments) {
+  final rawProducts = [
+    RawProductItem(
+      name: 'Персик',
+      categoryName: 'Растительная пища',
+      subcategoryName: 'Фрукты',
+      expirationDate: DateTime(2023, 12, 22),
+      qty: 5,
+    ),
+    RawProductItem(
+      name: 'Молоко',
+      categoryName: 'Молочные продукты',
+      subcategoryName: 'Напитки',
+      expirationDate: DateTime(2023, 12, 22),
+      qty: 5,
+    ),
+    RawProductItem(
+      name: 'Кефир',
+      categoryName: 'Молочные продукты',
+      subcategoryName: 'Напитки',
+      expirationDate: DateTime(2023, 12, 22),
+      qty: 5,
+    ),
+    RawProductItem(
+      name: 'Творог',
+      categoryName: 'Молочные продукты',
+      subcategoryName: 'Не напитки',
+      expirationDate: DateTime(2023, 12, 22),
+      qty: 0,
+    ),
+    RawProductItem(
+      name: 'Творожок',
+      categoryName: 'Молочные продукты',
+      subcategoryName: 'Не напитки',
+      expirationDate: DateTime(2023, 12, 22),
+      qty: 0,
+    ),
+    RawProductItem(
+      name: 'Творог',
+      categoryName: 'Молочные продукты',
+      subcategoryName: 'Не напитки',
+      expirationDate: DateTime(2023, 12, 22),
+      qty: 0,
+    ),
+    RawProductItem(
+      name: 'Гауда',
+      categoryName: 'Молочные продукты',
+      subcategoryName: 'Сыры',
+      expirationDate: DateTime(2023, 12, 22),
+      qty: 3,
+    ),
+    RawProductItem(
+      name: 'Маасдам',
+      categoryName: 'Молочные продукты',
+      subcategoryName: 'Сыры',
+      expirationDate: DateTime(2023, 12, 22),
+      qty: 2,
+    ),
+    RawProductItem(
+      name: 'Яблоко',
+      categoryName: 'Растительная пища',
+      subcategoryName: 'Фрукты',
+      expirationDate: DateTime(2023, 12, 4),
+      qty: 4,
+    ),
+    RawProductItem(
+      name: 'Морковь',
+      categoryName: 'Растительная пища',
+      subcategoryName: 'Овощи',
+      expirationDate: DateTime(2023, 12, 23),
+      qty: 51,
+    ),
+    RawProductItem(
+      name: 'Черника',
+      categoryName: 'Растительная пища',
+      subcategoryName: 'Ягоды',
+      expirationDate: DateTime(2023, 12, 25),
+      qty: 0,
+    ),
+    RawProductItem(
+      name: 'Курица',
+      categoryName: 'Мясо',
+      subcategoryName: 'Птица',
+      expirationDate: DateTime(2023, 4, 18),
+      qty: 2,
+    ),
+    RawProductItem(
+      name: 'Говядина',
+      categoryName: 'Мясо',
+      subcategoryName: 'Не птица',
+      expirationDate: DateTime(2023, 4, 17),
+      qty: 0,
+    ),
+    RawProductItem(
+      name: 'Телятина',
+      categoryName: 'Мясо',
+      subcategoryName: 'Не птица',
+      expirationDate: DateTime(2023, 12, 17),
+      qty: 0,
+    ),
+    RawProductItem(
+      name: 'Индюшатина',
+      categoryName: 'Мясо',
+      subcategoryName: 'Птица',
+      expirationDate: DateTime(2023, 12, 17),
+      qty: 0,
+    ),
+    RawProductItem(
+      name: 'Утка',
+      categoryName: 'Мясо',
+      subcategoryName: 'Птица',
+      expirationDate: DateTime(2023, 12, 18),
+      qty: 0,
+    ),
+    RawProductItem(
+      name: 'Гречка',
+      categoryName: 'Растительная пища',
+      subcategoryName: 'Крупы',
+      expirationDate: DateTime(2023, 12, 22),
+      qty: 8,
+    ),
+    RawProductItem(
+      name: 'Свинина',
+      categoryName: 'Мясо',
+      subcategoryName: 'Не птица',
+      expirationDate: DateTime(2023, 6, 23),
+      qty: 5,
+    ),
+    RawProductItem(
+      name: 'Груша',
+      categoryName: 'Растительная пища',
+      subcategoryName: 'Фрукты',
+      expirationDate: DateTime(2023, 6, 25),
+      qty: 5,
+    ),
+  ];
 
-  // Список товаров
-  final products = surf_dart_courses_template.convertToList(
-    articles,
-    surf_dart_courses_template.ProductListFromStringConverter(),
-  );
+  final List<RawProductItem> filteredRawProducts =
+      rawProducts.where((rawProduct) {
+    return rawProduct.expirationDate.compareTo(DateTime.now()) > 0 &&
+        rawProduct.qty > 0;
+  }).toList();
 
-  // Фильтр по категории
-  final filteredProductsByCategory = surf_dart_courses_template.applyFilter(
-    products,
-    surf_dart_courses_template.FilterProductByCategory('молоко'),
-  );
-  surf_dart_courses_template.printProductList(
-      filteredProductsByCategory, '\nКатегория "молоко"');
+  print(filteredRawProducts);
 
-  // Фильтр по цене
-  final filteredProductsByPrice = surf_dart_courses_template.applyFilter(
-    products,
-    surf_dart_courses_template.FilterProductByPriceNotHigher(200),
-  );
-  surf_dart_courses_template.printProductList(
-      filteredProductsByPrice, '\nТовары с ценой не больше 200 рублей');
+  final RawProductCatalog rawProductCatalog = {};
+  filteredRawProducts.fold(rawProductCatalog, (catalog, rawProduct) {
+    catalog
+        .putIfAbsent(
+            rawProduct.categoryName, () => <String, List<RawProductItem>>{})
+        .putIfAbsent(rawProduct.subcategoryName, () => <RawProductItem>[])
+        .add(rawProduct);
+    return catalog;
+  });
 
-  // Фильтр по остатку на складе
-  final filteredProductsByQuantity = surf_dart_courses_template.applyFilter(
-    products,
-    surf_dart_courses_template.FilterProductByQuantity(50),
-  );
-  surf_dart_courses_template.printProductList(
-      filteredProductsByQuantity, '\nТовары с остатком меньше 50 шт');
+  print(rawProductCatalog);
 }
--- task-1/lib/surf_dart_courses_template.dart
+++ task-1/lib/surf_dart_courses_template.dart
@@ -1,107 +1,24 @@
-class Product {
-  final int id;
-  final String category;
+/// Тип данных для каталога товаров в форме словаря
+typedef RawProductCatalog = Map<String, Map<String, List<RawProductItem>>>;
+
+/// Класс для описание товара
+class RawProductItem {
   final String name;
-  final double price;
+  final String categoryName;
+  final String subcategoryName;
+  final DateTime expirationDate;
+  final int qty;
 
-  final double quantity;
-
-  Product({
-    required this.id,
-    required this.category,
+  RawProductItem({
     required this.name,
-    required this.price,
-    required this.quantity,
+    required this.categoryName,
+    required this.subcategoryName,
+    required this.expirationDate,
+    required this.qty,
   });
-}
-
-abstract interface class ListDataConverter<T extends Object, V> {
-  List<T> convertToListData(V data);
-}
-
-class ProductListFromStringConverter
-    implements ListDataConverter<Product, String> {
-  const ProductListFromStringConverter();
 
   @override
-  List<Product> convertToListData(String data) {
-    var products = data
-        .split('\n')
-        .where((element) => element.trim().isNotEmpty)
-        .map((element) {
-      final rowData = element.split(',');
-
-      return Product(
-        id: int.parse(rowData[0]),
-        category: rowData[1],
-        name: rowData[2],
-        price: double.parse(rowData[3]),
-        quantity: double.parse(rowData[4]),
-      );
-    }).toList();
-
-    return products;
-  }
-}
-
-List<T> convertToList<T extends Object, V>(
-    V data, ListDataConverter<T, V> converter) {
-  return converter.convertToListData(data);
-}
-
-abstract interface class Filter<T> {
-  bool apply(T value);
-}
-
-class FilterProductByCategory implements Filter<Product> {
-  final String category;
-
-  FilterProductByCategory(this.category);
-
-  @override
-  bool apply(Product product) {
-    return product.category.trim() == category.trim();
-  }
-}
-
-class FilterProductByQuantity implements Filter<Product> {
-  final double quantity;
-
-  FilterProductByQuantity(this.quantity);
-
-  @override
-  bool apply(Product product) {
-    return product.quantity < quantity;
-  }
-}
-
-class FilterProductByPriceNotHigher implements Filter<Product> {
-  final double price;
-
-  FilterProductByPriceNotHigher(this.price);
-
-  @override
-  bool apply(Product product) {
-    return product.price <= price;
-  }
-}
-
-List<Product> applyFilter(List<Product> products, Filter<Product> filter) {
-  return products.where(filter.apply).toList();
-}
-
-abstract interface class IListDataConverter<T extends Object, V> {
-  List<T> convertToListData(V data);
-}
-
-void printProductList(List<Product> products, [String messageInfo = '']) {
-  if (messageInfo.isNotEmpty) {
-    print(messageInfo);
-  }
-
-  for (int i = 0; i < products.length; i++) {
-    final product = products[i];
-    print(
-        '${product.id}  ${product.category} ${product.name} ${product.price} рублей ${product.quantity} шт');
+  String toString() {
+    return name;
   }
 }
